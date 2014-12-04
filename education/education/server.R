899690fcc31e78574229c7b09b2fe5b8cae43d92
emikoroe <emikoroe@gmail.com>
2014-12-04 11:55:24 -0500

    added va and changed marital

---
+++ education/education/server.R
@@ -0,0 +1,254 @@
+#######################################
+## Title: Income server.R            ##
+## Author(s): Emily Ramos, Arvind    ##
+##            Ramakrishnan, Jenna    ##
+##            Kiridly, Steve Lauer   ## 
+## Date Created:  11/5/2014          ##
+## Date Modified: 11/7/2014          ##
+#######################################
+
+shinyServer(function(input, output, session) {
+  ## inc_df is a reactive dataframe. Necessary for when summary/plot/map have common input (Multiple Variables). Not in this project
+  inc_df <- reactive({
+    ## Filter the data by the chosen Five Year Range 
+    inc_df <- inc_data %>%
+      filter(Five_Year_Range == input$year) %>%
+      select(1:4, Five_Year_Range, Median_Household_Income, Margin_Error_Median)
+    
+    ## Output reactive dataframe
+    inc_df    
+  })
+  
+  ## Create summary table
+  output$summary <- renderDataTable({
+    ## Make reactive dataframe into regular dataframe
+    inc_df <- inc_df()
+    
+    ## make municipals a vector based on input variable
+    if(!is.null(input$sum_muni))
+      munis <- input$sum_muni
+    ## if none selected, put all municipals in vector
+    if(is.null(input$sum_muni))
+      munis <- MA_municipals
+    
+    ## if the user checks the meanUS box or the meanMA box, add those to counties vector
+    if(input$US_mean){
+      if(input$MA_mean){
+        munis <- c("United States", "MA", munis) ## US and MA  
+      } else{
+        munis <- c("United States", munis) ## US only
+      }
+    } else{
+      if(input$MA_mean){
+        munis <- c("MA", munis) ## US only ## MA only
+      }
+    }
+    #     browser( )
+    ## create a dataframe consisting only of counties in vector
+    inc_df <- inc_df %>%
+      filter(Region %in% munis) %>%
+      select(4:length(colnames(inc_df)))
+    
+    colnames(inc_df) <- gsub("_", " ", colnames(inc_df))
+    
+    inc_df[,3] <- prettyNum(inc_df[,3], big.mark=",")
+    inc_df[,4] <- prettyNum(inc_df[,4], big.mark=",")
+    
+    return(inc_df)
+  }, options = list(searching = FALSE, orderClasses = TRUE)) # there are a bunch of options to edit the appearance of datatables, this removes one of the ugly features
+  
+  ## create the plot of the data
+  ## for the Google charts plot
+  output$plot <- reactive({
+    ## make reactive dataframe into regular dataframe
+    inc_df <- inc_df()
+    
+    county <- as.character(inc_df$County[which(inc_df$Municipal==input$plot_muni)])
+    
+    ## make counties a vector based on input variable
+    munis <- c(input$plot_muni, county, "MA", "United States")
+    
+    muni_index <- c()
+    
+    for(i in 1:length(munis)){
+      muni_index[i] <- match(munis[i], inc_df$Region)
+    }
+#     browser()
+    plot_df <- inc_df[muni_index,] %>%
+      select(Region, Median_Household_Income)
+    
+    colnames(plot_df) <- gsub("_", " ", colnames(plot_df))
+    
+#     plot_df[,"pop.html.tooltip"] <- paste0("$", prettyNum(plot_df[,2], big.mark = ","))
+    
+    list(
+      data=googleDataTable(plot_df))
+  })
+  
+  
+  #####################################MAP CREATION##############
+  
+  ## set map colors
+  map_dat <- reactive({
+    
+    ## Browser command - Stops the app right when it's about to break
+    ## make reactive dataframe into regular dataframe
+    inc_df <- inc_df()
+    
+    ## take US, MA, and counties out of map_dat
+    map_dat <- inc_df %>%
+      filter(!is.na(Municipal))
+    
+    ## assign colors to each entry in the data frame
+    color <- as.integer(cut2(map_dat[,"Median_Household_Income"],cuts=cuts))
+    map_dat <- cbind.data.frame(map_dat, color)
+    map_dat$color <- ifelse(is.na(map_dat$color), length(map_colors), 
+                            map_dat$color)
+    map_dat$opacity <- 0.7
+    
+    ## find missing counties in data subset and assign NAs to all values
+    missing_munis <- setdiff(leftover_munis_map, map_dat$Region)
+    missing_df <- data.frame(Municipal = missing_munis, County = NA, State = "MA", 
+                             Region = missing_munis, Five_Year_Range = input$year, 
+                             Median_Household_Income = NA, Margin_Error_Median = NA,
+                             color=length(map_colors), opacity = 0)
+    
+    na_munis <- setdiff(MA_municipals_map, map_dat$Region)
+    na_df <- data.frame(Municipal = na_munis, County = NA, State = "MA", 
+                             Region = na_munis, Five_Year_Range = input$year, 
+                             Median_Household_Income = NA, Margin_Error_Median = NA,
+                             color=length(map_colors), opacity = 0.7)
+    
+    
+    # combine data subset with missing counties data
+    map_dat <- rbind.data.frame(map_dat, missing_df, na_df)
+    map_dat$color <- map_colors[map_dat$color]
+    return(map_dat)
+  })
+  
+  values <- reactiveValues(selectedFeature=NULL, highlight=c())
+  
+  #############################################
+  # observe({
+  #   values$highlight <- input$map_shape_mouseover$id
+  #   #browser()
+  # })
+  
+  # # Dynamically render the box in the upper-right
+  # output$countyInfo <- renderUI({
+  #   
+  #   if (is.null(values$highlight)) {
+  #     return(tags$div("Hover over a county"))
+  #   } else {
+  #     # Get a properly formatted state name
+  #     countyName <- names(MAcounties)[values$highlight]
+  #     return(tags$div(
+  #       tags$strong(countyName),
+  #       tags$div(density[countyName], HTML("people/m<sup>2</sup>"))
+  #     ))
+  #   }
+  # })
+  
+  # lastHighlighted <- c()
+  # # When values$highlight changes, unhighlight the old state (if any) and
+  # # highlight the new state
+  # observe({
+  # #   if (length(lastHighlighted) > 0)
+  # #     drawStates(getStateName(lastHighlighted), FALSE)
+  #   lastHighlighted <<- values$highlight
+  #   
+  #   if (is.null(values$highlight))
+  #     return()
+  #   
+  #   isolate({
+  #     drawStates(getStateName(values$highlight), TRUE)
+  #   })
+  # })
+  
+  ###########################################
+  
+  ## draw leaflet map
+  map <- createLeafletMap(session, "map")
+  
+  ## the functions within observe are called when any of the inputs are called
+  
+  ## Does nothing until called (done with action button)
+  observe({
+    input$action
+    
+    ## load in relevant map data
+    map_dat <- map_dat()
+    
+    ## All functions which are isolated, will not run until the above observe function is activated
+    isolate({
+      ## Duplicate MAmap to x
+      x <- MA_map_muni
+      #     browser()
+      ## for each county in the map, attach the Crude Rate and colors associated
+      for(i in 1:length(x$features)){
+        ## Each feature is a county
+        x$features[[i]]$properties["Median_Household_Income"] <- 
+          map_dat[match(x$features[[i]]$properties$NAMELSAD10, map_dat$Region), "Median_Household_Income"]
+        ## Style properties
+        x$features[[i]]$properties$style <- list(
+          fill=TRUE, 
+          ## Fill color has to be equal to the map_dat color and is matched by county
+          fillColor = map_dat$color[match(x$features[[i]]$properties$NAMELSAD10, map_dat$Region)], 
+          ## "#000000" = Black, "#999999"=Grey, 
+          weight=1, stroke=TRUE, 
+          opacity=map_dat$opacity[match(x$features[[i]]$properties$NAMELSAD10, map_dat$Region)], 
+          color="#000000", 
+          fillOpacity=map_dat$opacity[match(x$features[[i]]$properties$NAMELSAD10, map_dat$Region)])
+      }
+      
+      map$addGeoJSON(x) # draw map
+    })
+  })
+  
+  observe({
+    ## EVT = Mouse Click
+    evt <- input$map_click
+    if(is.null(evt))
+      return()
+    
+    isolate({
+      values$selectedFeature <- NULL
+    })
+  })
+  
+  observe({
+    evt <- input$map_geojson_click
+    if(is.null(evt))
+      return()
+    
+    isolate({
+      values$selectedFeature <- evt$properties
+    })
+  })
+  ##  This function is what creates info box
+  output$details <- renderText({
+    
+    ## Before a county is clicked, display a message
+    if(is.null(values$selectedFeature)){
+      return(as.character(tags$div(
+        tags$div(
+          h4("Click on a town or city"))
+      )))
+    }
+#     browser()
+    muni_name <- values$selectedFeature$NAMELSAD10
+    muni_value <- prettyNum(values$selectedFeature["Median_Household_Income"], big.mark = ",")
+    
+    ## If clicked county has no crude rate, display a message
+    if(muni_value == "NULL"){
+      return(as.character(tags$div(
+        tags$h5("Median Household Income in ", muni_name, "is not available for this timespan"))))
+    }
+    ## For a single year when county is clicked, display a message
+    as.character(tags$div(
+      tags$h4("Median Household Income in ", muni_name, " for ", input$year),
+      tags$h5("$",muni_value)
+    ))
+  })
+  
+})
---
+++ education/education/ui.R
@@ -0,0 +1,250 @@
+#######################################
+## Title: Income ui.R                ##
+## Author(s): Emily Ramos, Arvind    ##
+##            Ramakrishnan, Jenna    ##
+##            Kiridly, Steve Lauer   ## 
+## Date Created:  11/5/2014          ##
+## Date Modified: 11/7/2014          ##
+#######################################
+
+shinyUI(fluidPage(
+  ## HTML to create generate map button
+  gen_map_button,
+  ## this starts the googleCharts engine
+  googleChartsInit(),
+  
+  ## blank title, but put in a special title for window tab
+  titlePanel("", windowTitle = "SEIGMA: Household Income Shiny App"),
+  
+  ## Create sidebar
+  sidebarLayout(
+    sidebarPanel(width=4,
+      ## Conditional panel means if the condition is met show all text below otherwise Don't!
+      summary_side_text,
+      
+      plot_side_text,
+      
+      map_side_text,
+      
+      info_side_text,
+      
+      ## in map, allow for variable selection
+      
+      ## Choose range for year.
+      ## Initializing a single slider
+      conditionalPanel(
+        condition="input.tabs == 'summary' || input.tabs == 'plot' || input.tabs == 'map'",
+      selectInput("year", "Select Five Year Range",
+                  choices = list("2005-2009" = "2005-2009", "2006-2010" = "2006-2010",
+                                 "2007-2011" = "2007-2011"))
+      ),
+      
+
+      ## in summary, allow for municipal selection
+      conditionalPanel(
+        condition="input.tabs == 'summary'",
+        ## Select input = List
+        selectInput("sum_muni", "Select Municipality", 
+                    choices = MA_municipals,
+                    ## Multiple allows for multi-county selection
+                    multiple=TRUE)),
+      
+      ## in plot, allow for municipal selection
+      conditionalPanel(
+        condition="input.tabs == 'plot'",
+        ## Select input = List
+        selectInput("plot_muni", "Select Municipality", 
+                    choices = MA_municipals)),
+      
+      ## In summary, show boxes that will compare to MA or US average
+      conditionalPanel(
+        condition="input.tabs == 'summary'",
+        ## False at the end means it starts off unchecked
+        checkboxInput("MA_mean", "Compare to MA Average", FALSE),
+        checkboxInput("US_mean", "Compare to US Average", FALSE)
+      ),
+      
+      tags$hr(),
+      
+      ## author line
+      helpText("Created by Emily R. Ramos, Arvind Ramakrishnan, Jenna F. Kiridly, Sophie E. O'Brien and Stephen A. Lauer"),
+      
+      ## email feedback link
+      ## To develop a link in HTML
+      helpText(a("Send us your comments or feedback!", href="http://www.surveygizmo.com/s3/1832220/ShinyApp-Evaluation", target="_blank")),
+      
+      ## data source citation
+      helpText(a("Data Source: CDC Wonder", href="http://wonder.cdc.gov/wonder/help/cmf.html",
+                 target="_blank")),
+      
+      ## GitHub link
+      helpText(a("View our data and code on GitHub", 
+                 href="https://github.com/sEigmA/SEIGMA/tree/gh-pages/income", target="_blank")),
+      
+      helpText("If using Internet Explorer, application only visible in version 10.")
+    ),
+
+######### End of Sidebar  #########
+    
+######### Start of Main Panel #####
+    
+bootstrapPage(mainPanel(
+      ## put in logo for title
+      a(img(src = "logo.jpg", height=105, width=920), href="http://www.umass.edu/seigma/"),
+      
+      ## create tabs
+      tabsetPanel(
+        tabPanel("About", 
+                 ## strong=bold, p=paragraph, em=emboss/italicised or bold italicized, 
+                 about_main_text, value="about"),
+        
+        ## summary tab
+        tabPanel("Summary", 
+                 dataTableOutput("summary"), value="summary", 
+                 tags$style(type="text/css", '#summary tfoot {display:none;}')),
+        
+        ## plot tab with google chart options
+        tabPanel("Plot",
+                 ## make chart title here (otherwise not centered)
+                 h4("Average Annual Median Household Income (Inflation-Adjusted Dollars) of Population by Region Over Five Year Period", align="center"),
+                 ## make a row to put two charts in
+                 
+                 googleColumnChart("plot", width="100%", height="475px", options = list(
+                   ## set fonts
+                   fontName = "Source Sans Pro",
+                   fontSize = font_size,
+                   title = "",
+                   ## set axis titles, ticks, fonts, and ranges
+                   hAxis = list(
+                     title = "",
+                     textStyle = list(
+                       fontSize = font_size),
+                     titleTextStyle = list(
+                       fontSize = font_size+2,
+                       bold = TRUE,
+                       italic = FALSE)
+                   ),
+                   vAxis = list(
+                     title = "Median Household Income",
+                     viewWindow = ylim,
+                     textStyle = list(
+                       fontSize = font_size),
+                     titleTextStyle = list(
+                       fontSize = font_size+2,
+                       bold = TRUE,
+                       italic = FALSE)
+                   ),
+                   
+                   ## set legend fonts
+                   legend = list(
+                     position = "none"),
+                   
+                   ## set chart area padding
+                   chartArea = list(
+                     top = 50, left = 75,
+                     height = "75%", width = "70%"
+                   ),
+                   
+                   domain = list(
+                     role = c("domain", "data", "style")),
+                   
+                   ## set colors
+                   colors = cbbPalette[4:8],
+                   
+                   ## set point size
+                   pointSize = 3,
+                   
+                   ## set tooltip font size
+                   ## Hover text font stuff
+                   tooltip = list(
+                     textStyle = list(
+                       fontSize = font_size
+                     )
+                   )
+                 )),
+                 ## add text about the variables
+#                  plot_main_text,
+                 value="plot"),
+        
+        ## plot map
+        tabPanel("Map",
+                 
+                 ## Add a little CSS to make the map background pure white
+                 tags$head(tags$style("
+    #showcase-code-position-toggle, #showcase-sxs-code { display: none; }
+    .floater { background-color: white; padding: 8px; opacity: 1; border-radius: 6px; box-shadow: 0 0 15px rgba(0,0,0,0.2); }
+  ")),
+                 ## Map Creation
+                 leafletMap("map", width="100%", height=500, 
+                            options=list(center = c(42.15, -71.65), zoom=8, 
+                                         ##Bounds for the map for when zoomed in on mass
+                                         maxBounds = list(list(41, -73.5), 
+                                                          list(43, -70)))),
+                 ## Info Box 
+                 conditionalPanel(
+                   condition="input.action != 0",
+                   absolutePanel(left=450, top=450, width=300, class="floater",
+                                 htmlOutput("details"))),
+                 
+                 conditionalPanel(
+                   condition="input.tabs == 'map' && input.action == 0",
+                   ## within the map area, you can create an action button.  similar to initializing the legend but just putting a button instead.
+                   absolutePanel(right = 400, top = 300, class = "floater",
+                                 actionButton("action", "Generate Map")
+                 )),
+                 
+                 ## Legend
+                 conditionalPanel(
+                   condition="input.action != 0",
+                   absolutePanel(
+                     right = 30, top = 215, draggable=FALSE, style = "", 
+                     class = "floater",
+#                      strong("Crude Suicide Rate"),
+                     tags$table(
+                       mapply(function(from, to, color) {
+                         tags$tr(
+                           tags$td(tags$div(
+                             style = sprintf("width: 16px; height: 16px; background-color: %s;", color)
+                           )),
+                           tags$td("$", prettyNum(round(from), big.mark = ","), "to", "$", 
+                                   prettyNum(round(to), big.mark = ","), align = "right")
+                         )
+                       }, 
+                       colorRanges$from, colorRanges$to, map_colors[-length(map_colors)],
+                       SIMPLIFY=FALSE),
+                       tags$td(tags$div(
+                         style = sprintf("width: 16px; height: 16px; background-color: %s;", "#999999")
+                       )),
+                       tags$td("Data not available", align = "right")
+                     )
+                   )),
+                 
+#                plot_main_text,
+                 value="map"),
+        
+        tabPanel("More Info", 
+                 p(strong("Variable Summary:")),
+                 tags$br(),
+                 tags$ul(
+                   tags$li(p(strong("Median household income"), "provides a clear trend to assess resident's income overtime. Annual data for median household income was collected for a ten-year time series, from 2002- 2012, the latest data available.  Data was collected at multiple levels to allow for analysis at multiple levels; municipality, state, and US level comparatively.")),
+                   tags$br(),
+                   tags$li(p(strong("Median Household Income  (MHI)"),
+                             " : Average annual median household income in inflation-adjusted dollars over a five-year period for each municipality")),
+                   tags$br(),
+                   tags$li("When analyzing data sets per municipality five- year sets are used because estimates for smaller regions require a larger sample size than can be provided by single year data.")
+                   ),
+                 tags$br(),
+                 p("SEIGMA. Social and Economic Impacts of Gambling in Massachusetts, University of Massachusetts School of Public Health and Health Sciences. (2014). Report on the Social and Economic Impact of Gambling in Massachusetts SEIGMA Gambling study. Report to the Massachusetts Gaming Commission & the Massachusetts department of Public Health. Retrieved from:"), a("http://www.umass.edu/seigma/sites/default/files/March%202014%20SEIGMA%20Report_6-19_for%20website.pdf"),
+                 
+                 
+                 
+              
+                  
+                 
+                 ## email feedback link
+                 h3(a("Please fill out our survey to help improve the site!", href="http://www.surveygizmo.com/s3/1832220/ShinyApp-Evaluation", target="_blank")), value="info"),
+        id="tabs"
+      )
+    ))
+  )
+))
