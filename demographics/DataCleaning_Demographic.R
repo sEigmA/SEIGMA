9aa6f920b02d7e6cc19648cf4ca3a753364e88c7
xuelianli <xuelianli@umass.edu>
2015-01-30 00:05:55 -0500

    replace N/A +Assign value to Region

--- demographics/DataCleaning_Demographic.R
+++ demographics/DataCleaning_Demographic.R
@@ -4,8 +4,8 @@
 ##            Ramakrishnan, Jenna    ##
 ##            Kiridly, Steve Lauer   ##
 ##            Xuelian, Li            ##
-## Date Created:  11/5/2014          ##
-## Date Modified: 11/6/2014          ##
+## Date Created:  01/29/2015         ##
+## Date Modified: 01/29/2015         ##
 #######################################
 
 require(sas7bdat)
@@ -19,4 +19,15 @@ colnames(Demographic)[5:133] <- as.character(column_titles$Label[1:129])
 
 ## remove unnecessary characters from column names
 colnames(Demographic)[5:133] <- substring(colnames(Demographic[5:133]), first=12)
+colnames(Demographic)[5:133]<-gsub("; SEX AND AGE", "", colnames(Demographic[5:133]))
 
+Dem_data<-Demographic[,c(-(6:8), -11)]
+
+## Replace N/A's with "NA" to remove the slash.
+Dem_data2<-Dem_data
+Dem_data2[,1:4]<-apply(Dem_data2[,1:4],2, function(x) replace(x, x=="N/A", NA))
+
+Dem_data2$County  <- ifelse(!is.na(Dem_data2$County), paste(Dem_data2$County, "County"), NA)
+Dem_data2$Region  <- ifelse(!is.na(Dem_data2$Region), "United States", "MA")
+Dem_data2$Region  <- ifelse(!is.na(Dem_data2$County), Dem_data2$County, Dem_data2$Region)
+Dem_data2$Region  <- ifelse(!is.na(Dem_data2$Municipal),as.character(Dem_data2$Municipal),as.character(Dem_data2$Region))
