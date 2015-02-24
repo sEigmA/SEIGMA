c2ec1da5f445aa15292fbae00f0a41789a8690ad
arvindram12 <arvind.ramakrishnan76@gmail.com>
2015-02-24 17:42:13 -0500

    Functional Problem Gambling Rate Map code-generates image.
    Still need to add rates in each county, and export an image file.
    
    ~AR, 2/24/15

--- probgambdataratenew/ProbGambDataMapCode.R
+++ probgambdataratenew/ProbGambDataMapCode.R
@@ -7,14 +7,14 @@ require(ggplot2)
 require(ggmap)
 
 
-map("state", "MASSACHUSETTS")
+
 data(us.cities)
 
 ##Obtaining Problem Gambling Data
 pgdat <- read.csv(file="Problem_Gambling_data.csv")[,]
 head(pgdat)
 
-dat <- pgdat[c(1:14),c(1,6)]
+dat <- pgdat[c(1:14),c(1,7)]
 dat
 
 
@@ -29,19 +29,16 @@ mafips
 
 ##This gives the dataset with fips codes (location), 
 newdat <- cbind(mafips,dat)
-View(newdat)
+head(newdat)
 names(newdat)
 
 quants <- quantile(newdat$Problem.Gambling.Rate)
+quants
 
-leg.txt <- as.data.frame(quants)
 
 
-leg.txt2 <- c("<2%", "2-4%", "4-6%", "6-8%", "8-10%", ">10%")
-leg.txt
-leg.txt2
 
-legend("topright", leg.txt, horiz = TRUE, fill = colors)
+##legend("topright", leg.txt, horiz = TRUE, fill = colors)
 
 
 
@@ -52,27 +49,56 @@ legend("topright", leg.txt, horiz = TRUE, fill = colors)
 # Plot unemployment by country
 colors = c("#F1EEF6", "#D4B9DA", "#C994C7", "#DF65B0", "#DD1C77", "#980043")
 
- 
-
-#View(county.fips)
 
 
+# df<-dat
+# data.frame(lapply(df, function(V) {
+#   if (is.character(V)) return(tolower(v))
+#   else return(v)
+# }))
 
-unemp$colorBuckets <- as.numeric(cut(unemp$unemp, c(0, 2, 4, 6, 8, 
-                                                    10, 100)))
-colorsmatched <- unemp$colorBuckets[match(county.fips$fips, unemp$fips)]
 
-map("county", col = colors[colorsmatched], fill = TRUE, resolution = 0, 
+
+par(mfrow = c(1, 1))
+map("county", "MASSACHUSETTS")
+
+
+dat <- pgdat[c(1:14),c(1,7)]
+dat
+dat <- cbind(dat,mafips)
+
+mafips
+#data(unemp)
+#data(county.fips)
+
+# Plot unemployment by country
+colors = c("#F1EEF6", "#D4B9DA", "#C994C7", "#DF65B0", "#DD1C77", 
+           "#980043")
+head(unemp)
+
+
+dat$colorBuckets <- as.numeric(cut(dat$Problem.Gambling.Rate, c(0.39, 0.625, 1.25, 2.4, 3.30)))
+colorsmatched <- dat$colorBuckets[match(mafips$fips, dat$fips)]
+
+
+map("county", "MASSACHUSETTS", col = colors[colorsmatched], fill = TRUE, resolution = 0, 
     lty = 0, projection = "polyconic")
-
+## Loading required package: mapproj
 
 # Add border around each State
-map("state", col = "white", fill = FALSE, add = TRUE, lty = 1, lwd = 0.2, 
+map("county","MASSACHUSETTS", col = "black", fill = FALSE, add = TRUE, lty = 1, lwd = 0.2, 
     projection = "polyconic")
-title("unemployment by county, 2009")
+par(mar=c(4,4.5,2,1))
+par(oma=c(0,0,0,0))
+title("
+      Problem Gambling Rate 
+      by County in 2014")
 
-leg.txt <- c("<2%", "2-4%", "4-6%", "6-8%", "8-10%", ">10%")
-legend("topright", leg.txt, horiz = TRUE, fill = colors)
+#leg.txt <- c("<2%", "2-4%", "4-6%", "6-8%", "8-10%", ">10%")
+leg.txt <- c("0.40-0.625%", "0.625-1.25%","1.25-2.40", "2.40-3.30%")
 
-
+par(mar=c(1,2,,0))
+legend("bottomleft", leg.txt, horiz = FALSE, fill = colors,title="
+       Rate by quantile")
+ 
 
