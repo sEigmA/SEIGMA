3f3e317be90145a82f3c85c6c19eda51e0872a84
xuelianli <xuelianli@umass.edu>
2015-09-30 21:40:05 -0400

    data cleaning

--- unemployment/DataCleaning.R
+++ unemployment/DataCleaning.R
@@ -22,12 +22,12 @@ colnames(unemp)[6:12] <- substring(colnames(unemp[6:12]), first=12)
 
 ## grab only columns needed
 unemp <- unemp[,-c(7, 12)]
-write.csv(unemp, file="unempdata1.csv")
+write.csv(unemp, file="unempdata1.csv",row.names=FALSE)
 
 #Keep only Annual average for the year
 unemp_data <- unemp[which(unemp$Period=="Annual Average"),]
 
-write.csv(unemp_data, file="unempdata2.csv")
+write.csv(unemp_data, file="unempdata2.csv", row.names=FALSE)
 
 ## Replace N/A's with "NA" to remove the slash.
 unemp_data2 <- unemp_data
@@ -44,18 +44,22 @@ unemp_data2$County <- paste(unemp_data2$County, "County")
 unemp_data3 <- unemp_data2[,-6]
 
 ## save and reload to make factors into numeric (this is faster than the other methods)
-write.csv(unemp_data3, file="unempdata.csv")
-unemp_data3 <- read.csv("unempdata.csv")[,-c(1)]
+write.csv(unemp_data3, file="unempdata.csv", row.names=FALSE)
+unemp_data3 <- read.csv("unempdata.csv")
 
 colnames(unemp_data3)[c(5:9)] <- c("Year", "Unemployment Rate Avg", "No Unemployed Avg", "No Employed Avg", "No Labor Avg")
 
 #Organizing by Region
 unemp_data4 <- unemp_data3 %>%
-  arrange(Region)
+    arrange(Region)
 
 # Put US and MA at top of data set
 idx_unemp_MA <- which(unemp_data4$Region == "MA")
 idx_unemp_US <- which(unemp_data4$Region == "United States")
+MA<-unemp_data4[idx_unemp_MA,]
+US<-unemp_data4[idx_unemp_US,]
+write.csv(MA, file="MA.csv", row.names=FALSE)
+write.csv(US, file="US.csv", row.names=FALSE)
 unemp_data4 <- rbind.data.frame(unemp_data4[idx_unemp_US,], unemp_data4[idx_unemp_MA,], unemp_data4[-c(idx_unemp_MA, idx_unemp_US),])
 
 
