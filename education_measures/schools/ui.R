716e57328ed83a03411b8f767f4155b961d49ce8
JWB1990 <justin.wheeler.baldwin@gmail.com>
2016-04-04 00:54:46 -0400

    same

--- education_measures/schools/ui.R
+++ education_measures/schools/ui.R
@@ -122,7 +122,7 @@ shinyUI(fluidPage(
                                               "Native American" = "Native American",
                                               "Native Hawaiian/Pacific Islander" = "Native Hawaiian/Pacific Islander",
                                               "Multi-Race Non-Hispanic" = "Multi-Race/Non-Hispanic"),
-                                          selected = NULL)),
+                                          selected = "African American")),
              
              conditionalPanel("input.lmap_radio=='Gender'",
                               selectInput("lmap_level2","Choose Gender to map",
